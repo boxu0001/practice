e27aac9eb28be1403a92dc7b285a028629c343d0
box <lifesphere15@gmail.com>
2020-06-30 21:44:38 +1000

    S215

--- py3/S215_KthLargestEle.py
+++ py3/S215_KthLargestEle.py
@@ -42,4 +42,61 @@ class Solution:
         for i in range(k-1):
             heapq.heappop(pq)
         return -heapq.heappop(pq)
+
+#following are manually implemented Priority Queue
+    def __init__(self):
+        super().__init__()
+        self.pq = []
+
+    def pushPQ(self, ele):
+        self.pq += [ele]
+        self.pqUpLast()
+
+    def popPQ(self):
+        if len(self.pq) > 0:
+            self.pq[0], self.pq[-1] = self.pq[-1], self.pq[0]
+            first = self.pq.pop()
+            self.pqDownFirst()
+            return first
+        else:
+            return None
+
+    def pqDownFirst(self):
+        i = 0
+        ls = len(self.pq)
+        while i < ls:
+            if 2*i+1 < ls:
+                nxti = 2*i+1 if 2*i+2 >= ls or self.pq[2*i+2] < self.pq[2*i+1] else 2*i+2
+                if self.pq[i] < self.pq[nxti]:
+                    self.pq[i], self.pq[nxti] = self.pq[nxti], self.pq[i]
+                    i = nxti 
+                else:
+                    return
+            else:
+                return
+
+
+    def pqUpLast(self):
+        ls = len(self.pq)
+        i = ls-1
+        pi = (i-1) >> 1
+        while pi >= 0 and self.pq[i] > self.pq[pi]:
+            self.pq[i], self.pq[pi] = self.pq[pi], self.pq[i]
+            i = pi
+        
+
 #用priority queue 是核心        
+s=Solution()
+s.pushPQ(5)
+s.pushPQ(3)
+s.pushPQ(11)
+s.pushPQ(9)
+s.pushPQ(2)
+s.pushPQ(7)
+
+s.popPQ()
+s.popPQ()
+s.popPQ()
+s.popPQ()
+s.popPQ()
+

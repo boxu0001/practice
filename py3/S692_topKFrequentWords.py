6ca4d7caaa8c15abe1e13e27ea04ee6ac63ff0df
box <lifesphere15@gmail.com>
2020-10-07 23:53:59 +1100

    Update S692

--- py3/S692_topKFrequentWords.py
+++ py3/S692_topKFrequentWords.py
@@ -30,28 +30,104 @@ Follow up:
     Try to solve it in O(n log k) time and O(n) extra space.
 '''
 from __future__ import annotations
-import heapq as pq
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
         lens=len(words)
         wordcnt={}
-        queue=[] #space O(n)
+        queue=[] #space O(k)
+        
+        for w in words:
+            if w not in wordcnt:
+                wordcnt[w] = 0
+            else:
+                wordcnt[w] +=1
+        #time O(n)
+        
+        for w in wordcnt:
+            newitem = (wordcnt[w], w)
+            if len(queue) < k:
+                self.pushqueue(queue, newitem)
+            elif len(queue) == k and self.compareitems(queue[0], newitem) < 0:
+                self.replacetop(queue, newitem)
+            else:
+                pass
+        #time O(nlog(k))
+        result=[]
+        while queue:
+            _, w = queue[0]
+            result+=[w]
+            if len(queue) > 1:
+                self.replacetop(queue, queue.pop())
+            else:
+                break
+            
+        return result[::-1]
+    
+    def pushqueue(self, queue: List, item: (int, str)):
+        queue+=[item]
+        ci = len(queue)-1
+        pi = (ci-1)//2
+        while pi >=0:
+            pitem = queue[pi]
+            citem = queue[ci]
+            if self.compareitems(citem, pitem) < 0:
+                queue[ci] = pitem
+                queue[pi] = citem
+                ci = pi
+                pi = (ci-1)//2
+            else:
+                break
+        return
+
+    def replacetop(self, queue: List, item: (int, str)):
+        queue[0] = item
+        pi = 0
+        K = len(queue)
+        while pi < K:
+            c1 = pi*2+1
+            c2 = pi*2+2
+            small=c1 if c1 < K else None
+            if c2 < K:
+                small= c1 if self.compareitems(queue[c1], queue[c2]) < 0 else c2
+            if small != None and self.compareitems(queue[small], queue[pi]) < 0:
+                smallitem = queue[small]
+                queue[small] = queue[pi]
+                queue[pi] = smallitem
+                pi = small
+            else:
+                break
+        return
+
+    def compareitems(self, item1, item2):
+        (c_val, c_wd) = item1
+        (p_val, p_wd) = item2
+        if c_val < p_val or (c_val == p_val and c_wd > p_wd):
+            return -1
+        else:
+            return 1
+        
+
+    # simply using heapq, NOTICE it's O(nlog(n))
+    import heapq as pq
+    def topKFrequent_simple(self, words: List[str], k: int) -> List[str]:
+        lens=len(words)
+        wordcnt={}
+        queue=[]
         
         for w in words:
             if w not in wordcnt:
                 wordcnt[w] = lens
             else:
                 wordcnt[w] -=1
-        #time O(n)
         
         for w in wordcnt:
             pq.heappush(queue, (wordcnt[w], w))
-        #time O(nlog(n))
-
+        
         result=[]
         for i in range(k):
             result+=[pq.heappop(queue)[1]]
         
         return result
+
 s=Solution()
 s.topKFrequent(["love", "leetcode", "a", "love", "coding", "a"], 2)

cdd31691d526f0605e0637d3a5846e0b7d6d0047
box <lifesphere15@gmail.com>
2020-10-22 23:35:45 +1100

    Add New

--- py3/S943_shortestString.py
+++ py3/S943_shortestString.py
@@ -70,6 +70,59 @@ class Solution:
             xi = pi            
         return result
 
+    
+    # second approach, seem Greedy Algo
+    def shortestSuperstring2(self, A: List[str]) -> str:
+        """
+        :type A: List[str]
+        :rtype: str
+        """
+        A_len = len(A)
+        
+        while A_len != 1:
+            p, q = -1, -1
+            res = ''
+            max_val = float("-inf") 
+            for i in range(A_len):
+                for j in range(i+1, A_len):
+                    r, tmp_res = self.findOverlappingPair(A[i], A[j])
+                    if max_val < r:
+                        max_val = r
+                        res = tmp_res
+                        p = j
+                        q = i
+
+            A_len -= 1
+            if max_val == float("-inf"): 
+                A[0] = A[0] + A[A_len]
+            else:
+                A[p] = res
+                A[q] = A[A_len]
+            
+        return A[0]
+
+    def findOverlappingPair(self, s1, s2):
+        max_overlap_len = float("-inf") 
+        n = min(len(s1), len(s2))
+        res = ''
+
+        for i in range(1, n+1):
+            if s1.endswith(s2[:i]):
+                if max_overlap_len < i:
+                    max_overlap_len = i
+                    res = s1 + s2[i:]
+
+        for i in range(1, n+1):
+            if s2.endswith(s1[:i]):
+                if max_overlap_len < i:
+                    max_overlap_len = i
+                    res = s2 + s1[i:]
+
+        return max_overlap_len, res
+
+
+
+
 s=Solution()
 # s.shortestSuperstring(["alex","loves","leetcode"])
-s.shortestSuperstring(["catg","ctaagt","gcta","ttca","atgcatc"])
+s.shortestSuperstring2(["catg","ctaagt","gcta","ttca","atgcatc"])
